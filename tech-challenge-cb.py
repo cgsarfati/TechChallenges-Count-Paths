2806a1f8467a350d2171f489d9e16f1c229ba58f
vagrant <vagrant@vagrant.vm>
2017-10-25 05:54:36 +0000

    uploaded matrix problem with doc test

---
+++ tech-challenge-cb.py
@@ -0,0 +1,18 @@
+def number_of_paths(m, n):
+    """Find total amount of paths from upper left to bottom right of mxn matrix.
+    You can only move to the right or down of the cell. Return number
+    of paths as an integer via recursion.
+
+    >>> number_of_paths(3, 3)
+    6
+
+    >>> number_of_paths(4, 4)
+    20
+
+    """
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** All tests passed ***\n"
