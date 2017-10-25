ebcb7d098b4b80d479bbf998ea1b4a390e9cf163
vagrant <vagrant@vagrant.vm>
2017-10-25 07:02:14 +0000

    solved with non-recursive way

--- tech-challenge-cb.py
+++ tech-challenge-cb.py
@@ -1,3 +1,7 @@
+"""Find total amount of paths from upper left to bottom right of mxn matrix.
+    You can only move to the right or down of the cell. Return number
+    of paths as an integer via recursion."""
+
 # SOLUTION
 # 3x3 matrix
 
@@ -13,9 +17,7 @@
 
 
 def number_of_paths_recursion(m, n):
-    """Find total amount of paths from upper left to bottom right of mxn matrix.
-    You can only move to the right or down of the cell. Return number
-    of paths as an integer via recursion.
+    """Recursive sol'n with exponential runtime.
 
     >>> number_of_paths_recursion(3, 3)
     6
@@ -36,7 +38,7 @@ def number_of_paths_recursion(m, n):
 
 
 def number_of_paths_optimized(m, n):
-    """ Runtime solution of O(mn).
+    """ Runtime solution of O(mn) r/t nested for loop.
 
     >>> number_of_paths_optimized(3, 3)
     6
@@ -47,6 +49,25 @@ def number_of_paths_optimized(m, n):
     >>> number_of_paths_optimized(2, 2)
     2
     """
+
+    # initialize matrix to store sum values; start with all 0s
+    count = [[0 for x in range(m)] for y in range(n)]
+
+    # count paths for 1st column
+    for i in range(m):
+        count[i][0] = 1
+
+    # count paths for 1st row
+    for j in range(n):
+        count[0][j] = 1
+
+    # count paths for other cells by adding left and upper value in matrix
+    # fills from top left to bottom right
+    for i in range(1, m):
+        for j in range(n):
+            count[i][j] = count[i-1][j] + count[i][j-1]
+
+    return count[m-1][n-1]
 
 
 if __name__ == '__main__':
